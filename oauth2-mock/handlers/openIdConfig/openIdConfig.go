ddd92bb8bdbeb1bf20bca4d66ee71e6f54f3a590
Chwila <bartosz.chwila@sap.com>
2024-06-07 14:01:02 +0200

    Update oauth2 mock image paths

--- oauth2-mock/handlers/openIdConfig/openIdConfig.go
+++ oauth2-mock/handlers/openIdConfig/openIdConfig.go
@@ -12,7 +12,9 @@ type Handler struct {
 
 func (h Handler) Handle(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
-		"issuer":   h.Iss,
-		"jwks_uri": fmt.Sprintf("%s/oauth2/certs", h.Iss),
+		"issuer":                 h.Iss,
+		"jwks_uri":               fmt.Sprintf("%s/oauth2/certs", h.Iss),
+		"token_endpoint":         fmt.Sprintf("%s/oauth2/token", c.Request.Host),
+		"introspection_endpoint": fmt.Sprintf("%s/oauth2/introspect", h.Iss),
 	})
 }
