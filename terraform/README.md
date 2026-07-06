4eacfc2ba6a973f3e0e2cbc94a9f1539ec752182
Bartosz Chwila <103247439+barchw@users.noreply.github.com>
2026-07-06 08:10:52 +0200

    Setup terraform for httpbin hosting (#48)
    * Setup TF for httpbin hosting
    
    * format
    
    * Add README
    
    * Remove lint job

---
+++ terraform/README.md
@@ -0,0 +1,4 @@
+## Terraform setup for httpbin
+
+- Current directory: contains Terraform configuration files for deploying httpbin service.
+- Bootstrap directory: contains Terraform configuration files for creating a bucket to store Terraform state files.
---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+resource "google_cloud_run_v2_service" "httpbin" {
+  name                 = var.service_name
+  location             = var.region
+  ingress              = "INGRESS_TRAFFIC_ALL"
+  invoker_iam_disabled = true
+
+  scaling {
+    manual_instance_count = null
+    scaling_mode          = "AUTOMATIC"
+  }
+
+  template {
+    scaling {
+      min_instance_count = var.min_instance_count
+      max_instance_count = var.max_instance_count
+    }
+
+    containers {
+      image = var.image
+
+      ports {
+        container_port = var.container_port
+      }
+    }
+  }
+}
+
+data "google_dns_managed_zone" "zone" {
+  name = var.dns_zone_name
+}
+
+resource "google_cloud_run_domain_mapping" "httpbin" {
+  location = var.region
+  name     = var.custom_domain
+
+  metadata {
+    namespace = var.project_id
+  }
+
+  spec {
+    route_name = google_cloud_run_v2_service.httpbin.name
+  }
+}
+
+resource "google_dns_record_set" "httpbin" {
+  managed_zone = data.google_dns_managed_zone.zone.name
+  name         = "${var.custom_domain}."
+  type         = "CNAME"
+  ttl          = 300
+  rrdatas      = ["ghs.googlehosted.com."]
+
+  depends_on = [google_cloud_run_domain_mapping.httpbin]
+}
